1c412024244a062499e6f09bbd2deb05a954fbf2
stelzo <stelzo@steado.de>
2025-01-14 19:09:48 +0100

    fix not null

--- migrations/002_alm_xp.up.sql
+++ migrations/002_alm_xp.up.sql
@@ -1,8 +1,8 @@
 alter table almanax
-add column experience_ratio float not null;
+add column experience_ratio float not null default 0;
 
 alter table almanax
-add column optimal_level integer not null;
+add column optimal_level integer not null default 0;
 
 alter table almanax
-add column duration float not null;
+add column duration float not null default 0;
